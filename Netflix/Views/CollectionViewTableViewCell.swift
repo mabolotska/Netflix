c361e7f4efb4103c461f973c39bb71ca718807af
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-31 17:57:57 +0200

    header view and collectionviewcell

---
+++ Netflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix
+//
+//  Created by Maryna Bolotska on 31/07/23.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Netflix/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix
+//
+//  Created by Maryna Bolotska on 31/07/23.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
