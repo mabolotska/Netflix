8c036d353d7302b0d2acc8763def0454e43e5631
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-08-17 15:42:11 +0200

    Cell added

--- Netflix/Controllers/HomeViewController.swift
+++ Netflix/Controllers/HomeViewController.swift
@@ -7,9 +7,18 @@
 
 import UIKit
 
+enum Sections: Int {
+    case TrendingMovies = 0
+    case TrendingTv = 1
+    case Popular = 2
+    case Upcoming = 3
+    case TopRated = 4
+}
+
+
 class HomeViewController: UIViewController {
     
-    let sectionTitles: [String] = ["Trending Movies", "Popular", "Trending Tv", "Upcoming Movies", "Top rated"]
+    let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top rated"]
     
     private let homeFeedTable: UITableView = {
            let table = UITableView(frame: .zero, style: .grouped)
@@ -22,7 +31,7 @@ class HomeViewController: UIViewController {
 
         view.backgroundColor = .systemBackground
         
-        getTrendingMovies()
+        
         configureNavbar()
         
         view.addSubview(homeFeedTable)
@@ -52,20 +61,7 @@ class HomeViewController: UIViewController {
             homeFeedTable.frame = view.bounds
         }
   
-    private func getTrendingMovies() {
-
-        APICaller.shared.getTrendingMovies { results in
-            switch results {
-
-            case .success(let movies):
-                print(movies)
-            case .failure(let error):
-                print(error)
-
-            }
-        }
-    }
-
+ 
 
 }
 
@@ -85,6 +81,63 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
             return UITableViewCell()
         }
+        
+        switch indexPath.section {
+                case Sections.TrendingMovies.rawValue:
+                    APICaller.shared.getTrendingMovies { result in
+                        switch result {
+
+                        case .success(let titles):
+                            cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    }
+
+
+
+                case Sections.TrendingTv.rawValue:
+                    APICaller.shared.getTrendingTvs { result in
+                        switch result {
+                        case .success(let titles):
+                            cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    }
+                case Sections.Popular.rawValue:
+                    APICaller.shared.getPopular { result in
+                        switch result {
+                        case .success(let titles):
+                            cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    }
+                case Sections.Upcoming.rawValue:
+
+                    APICaller.shared.getUpcomingMovies { result in
+                        switch result {
+                        case .success(let titles):
+                            cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    }
+
+                case Sections.TopRated.rawValue:
+                    APICaller.shared.getTopRated { result in
+                        switch result {
+                        case .success(let titles):
+                            cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                default:
+                    return UITableViewCell()
+
+                }
 
         return cell
     }
@@ -116,6 +169,6 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
           header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
           header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
           header.textLabel?.textColor = .white
-          header.textLabel?.text = header.textLabel?.text?.lowercased()
+        header.textLabel?.text = header.textLabel?.text?.capitalizeFirstLetter()
       }
 }
---
+++ Netflix/ViewModels/TitleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitleViewModel.swift
+//  Netflix
+//
+//  Created by Maryna Bolotska on 17/08/23.
+//
+
+import Foundation
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
